a19676eb623c40e51bde4091883507ab020aaf9d
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-10-12 17:04:15 +0200

    1st version eml exercise using "For"

--- js/script.js
+++ js/script.js
@@ -5,25 +5,37 @@ btnCheck.addEventListener('click', function(){
     const users = ['giorgio@gmail.com', 'clelia@gmail.com', 'stefano@gmail.com', 'marco@gmail.com'];
     const emlCheck = document.querySelector('input').value;
     console.log(emlCheck)
-    console.log(users)
+    // console.log(users)
     let checkMex = document.getElementById('check-mex')
-    //inserisco check
-                // for (let i = 0; i < users.length; i++) {
-                //     // const element = users[i];
-                //     console.log(users(i));
+    
+    // const confirm = document.createElement('div')
+    // if(users.includes(emlCheck)){
+    //     checkMex.classList.remove('d-none');
+    //     checkMex.innerHTML = 'Presente!';
+    //     console.log(checkMex)
 
-                // }
-    const confirm = document.createElement('div')
-    if(users.includes(emlCheck)){
-        checkMex.classList.remove('d-none');
-        checkMex.innerHTML = 'Presente!';
-        // confirm.innerHTML('Presente!')
-        console.log(checkMex)
-
-    }else{
-        checkMex.classList.remove('d-none');
-        checkMex.innerHTML = 'La mail indicata non è registrata!';
-        console.log(checkMex)
+    // }else{
+    //     checkMex.classList.remove('d-none');
+    //     checkMex.innerHTML = 'La mail indicata non è registrata!';
+    //     console.log(checkMex)
+    // }
+//inserisco check
+    for (let i = 0; i < users.length; i++) {
+        let userCorrente = users[i];
+        if(userCorrente === emlCheck){
+            checkMex.classList.remove('d-none');
+            checkMex.innerHTML = 'Presente!';
+            // console.log(checkMex)
+        } else{
+            checkMex.classList.remove('d-none');
+            checkMex.innerHTML = 'La mail indicata non è registrata!';
+            // console.log(checkMex)
+        }
+    
     }
-
 })
+
+//utility
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
